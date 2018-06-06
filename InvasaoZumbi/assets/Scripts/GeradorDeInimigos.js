8b9f30049078ea8022545094e610283d207905be
LarioDiniz <lario.diniz@palo.com.br>
2018-06-06 12:45:48 -0300

    Parte03 - Aula 05
    Parte03 - Aula 05

--- InvasaoZumbi/assets/Scripts/GeradorDeInimigos.js
+++ InvasaoZumbi/assets/Scripts/GeradorDeInimigos.js
@@ -2,24 +2,50 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        raio: cc.Float,
         tempoParaGerar: cc.Float,
-        inimigoPrefab: cc.Prefab
+        inimigoPrefab: cc.Prefab, 
+        
+        _distanciaMinima: cc.Float
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    onLoad () {},
+    onLoad () {
+        this.schedule(this.gerar, this.tempoParaGerar);
+        let resolucao = cc.director.getVisibleSize();
+        let metadeDaLargura = resolucao.width / 2;
+        this._distanciaMinima = metadeDaLargura;
+    },
 
     start () {
-        this.schedule(this.gerar, this.tempoParaGerar);
+        
     },
 
     // update (dt) {},
 
     gerar(){
-        let zumbi = cc.instantiate(this.inimigoPrefab);
-        zumbi.parent = this.node.parent;
-        zumbi.position = this.node.position;
+        if (this.possoGerar()){
+            let posicao = this.calcularPosicao();
+            let zumbi = cc.instantiate(this.inimigoPrefab);
+            zumbi.parent = this.node.parent;
+            zumbi.position = posicao;
+        };
+    },
 
+    calcularPosicao () {
+        let alcance = new cc.Vec2(Math.random() - 0.5, Math.random() - 0.5);
+        alcance = alcance.normalize();
+        alcance = alcance.mul(this.raio * Math.random());
+        let posicao = this.node.position.add(alcance);
+
+        return posicao;        
+    },
+
+    possoGerar(){
+        distanciaAtual = this.node.position.sub(cc.Camera.main.node.position);
+        distanciaAtual = distanciaAtual.mag();
+        longeOSuficiente = distanciaAtual > this._distanciaMinima;
+        return longeOSuficiente;
     }
 });
--- InvasaoZumbi/assets/Scripts/Inimigo.js
+++ InvasaoZumbi/assets/Scripts/Inimigo.js
@@ -5,7 +5,9 @@ cc.Class({
         _alvo: cc.Node,        
         _movimentacao: cc.Component,
         _controleAnimacao: cc.Component,
+        _cronometroAtaque: cc.Float,
         
+        tempoAtaque: cc.Float,
         distanciaDeAtaque : cc.Float,
         dano: cc.Float
     },
@@ -17,6 +19,11 @@ cc.Class({
         this._movimentacao = this.getComponent("Movimentacao");
         this._controleAnimacao = this.getComponent("ControleDeAnimacao");
         this.node.on("SofrerDano", this.morrer, this);
+        this.resetCronometroDeAtaque();
+    },
+
+    resetCronometroDeAtaque (){
+        this._cronometroAtaque = this.tempoAtaque;
     },
 
     morrer () {
@@ -28,16 +35,19 @@ cc.Class({
 
     update (deltaTime) {
         let direcao = this._alvo.position.sub(this.node.position);
-
         let distancia = direcao.mag();
 
         this._movimentacao.setDirecao(direcao);
         this._movimentacao.andarParaFrente();   
         this._controleAnimacao.mudaAnimacao(direcao, "Andar");
 
-        if (distancia < this.distanciaDeAtaque){
+        this._cronometroAtaque -= deltaTime;
+        if (distancia < this.distanciaDeAtaque && this._cronometroAtaque <= 0){
             this._alvo.emit("SofreDano", {dano: this.dano});
         }
 
+        if (this._cronometroAtaque <= 0){
+            this.resetCronometroDeAtaque();
+        }
     },
 });
